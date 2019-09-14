dcad98837cd22c81eba9bead36c8d211972f34f8
Cheikh Mbow <cheikh2mbow@gmail.com>
2019-09-14 16:35:32 +0000

    supprimer

---
+++ supprimer.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style/bootstrap.min.css">
+    <link rel="stylesheet" href="style/js.css">
+    <link rel="stylesheet" href="style/sup.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="sup">
+    <p> Voulez-vous vraiment supprimer cet employer...</p><br>
+    &emsp;&emsp; &emsp; &ensp; <a href="sp.php?ma=<?= $_GET['ma']?>" class="btn btn-danger">Supprimer</a>  <a href="lister.php" class="btn btn-warning">Retour</a>  
+    </div>
+</body>
+</html>
