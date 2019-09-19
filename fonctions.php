1dfc456c9ebe8fd573c22b9ebe5ef098660f741d
Cheikh Mbow <cheikh2mbow@gmail.com>
2019-09-19 10:18:38 +0000

    der2

--- fonctions.php
+++ fonctions.php
@@ -3,3 +3,14 @@
 function debug($variable){
     echo '<pre>' .print_r($variable, true). '</pre>';
 }
+
+/*
+*retourne une connexion à la base de donnée
+*
+*@return PDO
+*/
+function getConnexion():PDO{
+    $connexion = new PDO('mysql:host = localhost;dbname=formulaire', 'cheikh1', 'abcdef');
+
+    return $connexion;
+}
